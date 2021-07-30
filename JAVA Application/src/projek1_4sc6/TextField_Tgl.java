f0c1a42df720df5481c151b163b2612be40c2516
muhammad habibi <muhhabibi46@gmail.com>
2021-07-30 21:28:35 +0700

    JAVA Windows Application

---
+++ JAVA Application/src/projek1_4sc6/TextField_Tgl.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projek1_4sc6;
+
+/**
+ *
+ * @author Inceptors
+ */
+class TextField_Tgl {
+
+    static Object getDate() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
